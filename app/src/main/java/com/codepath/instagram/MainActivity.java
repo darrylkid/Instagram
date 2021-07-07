c4e5d1426f692338057b8cf81000ac5ec13e0962
darrylkid <darrylbaailey@gmail.com>
2021-07-07 15:59:53 -0700

    Add button, intent, and layout for creating a new post.

--- app/src/main/java/com/codepath/instagram/MainActivity.java
+++ app/src/main/java/com/codepath/instagram/MainActivity.java
@@ -25,16 +25,36 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 ParseUser.logOut();
                 Log.i(TAG, "Logout successful");
-                goToLoginIntent();
+                goToLoginActivity();
             }
         };
         btnLogout.setOnClickListener(logoutHandler);
+
+        // Create an onClick listener for the post button.
+        Button btnCreatePost = findViewById(R.id.btnCreatePost);
+        View.OnClickListener createPostHandler = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // When button is clicked, go to the Post activity
+                // to create a new post.
+                goToPostActivity();
+            }
+        };
+        btnCreatePost.setOnClickListener(createPostHandler);
+
     }
 
 
-    public void goToLoginIntent() {
+    public void goToLoginActivity() {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void goToPostActivity() {
+        Intent intent = new Intent(MainActivity.this, Post.class);
+        startActivity(intent);
+        // We don't include finish here so that the user can go back to the
+        // main activity.
     }
 }
