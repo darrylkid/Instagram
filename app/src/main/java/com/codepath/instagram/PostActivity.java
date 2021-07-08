875b8d85ae95cd5d630f5d1c2e451266de5cd8bc
darrylkid <darrylbaailey@gmail.com>
2021-07-08 16:39:14 -0700

    Allow the user to take a picture, see the picture on the Image View, and post the picture to the database.

--- app/src/main/java/com/codepath/instagram/PostActivity.java
+++ app/src/main/java/com/codepath/instagram/PostActivity.java
@@ -1,9 +1,15 @@
 package com.codepath.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,20 +19,29 @@ import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 public class PostActivity extends AppCompatActivity {
+    // Constants
     private final static String TAG = "PostActivity";
+    private final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1;
 
+    // Views
     private Button btnSubmitPost;
     private Button btnTakePicture;
     private EditText etDescription;
     private ImageView ivImagePost;
+
+    // Camera
+    private File photoFile;
+    private String photoFileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +54,18 @@ public class PostActivity extends AppCompatActivity {
         etDescription = findViewById(R.id.etDescription);
         ivImagePost = findViewById(R.id.ivImagePost);
 
+        // Initialize the camera-related variables.
+        photoFile = null;
+        photoFileName = "photo.jpg";
+
+
         // Create onClick listener for the "Take Picture" button.
         View.OnClickListener takePictureHandler = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Launch the camera/image gallery action to set the empty
                 // image view to whatever the user selected.
-                queryPosts();
+                launchCamera();
             }
         };
         btnTakePicture.setOnClickListener(takePictureHandler);
@@ -54,24 +74,93 @@ public class PostActivity extends AppCompatActivity {
         View.OnClickListener submitPostHandler = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Package the data by using the Parcel API into the intent
-                // and go back to the main activity.
-
-                // Grab the description, current user, and image.
+                // Grab the description, current user, and photo file (image provided
+                // by user when the "Take Picture" button is clicked).
                 String description = etDescription.getText().toString();
                 ParseUser user = ParseUser.getCurrentUser();
-                savePost(description, user);
+
+                // Error handling for an invalid description.
+                if (etDescription.getText().toString().isEmpty()) {
+                    String errorMsg = "No description."
+                                    +  " Please add a description";
+                    Toast.makeText(PostActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Error handling for a user submitting a post with no photo.
+                if (photoFile == null || ivImagePost.getDrawable() == null) {
+                    String errorMsg = "No picture. Please take a picture.";
+                    Toast.makeText(PostActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Save the post to the database.
+                savePost(description, user, photoFile);
                 goToMainActvity();
             }
         };
         btnSubmitPost.setOnClickListener(submitPostHandler);
     }
 
-    public void savePost(String description, ParseUser currentUser) {
+    public void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(PostActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    public File getPhotoFileUri(String photoFileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.e(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + photoFileName);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivImagePost.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+    public void savePost(String description, ParseUser currentUser, File photoFile) {
         // Save the post to the database.
         Post post = new Post();
         post.setDescription(description);
         post.setUser(currentUser);
+        post.setImage(new ParseFile(photoFile));
         post.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
@@ -83,7 +172,10 @@ public class PostActivity extends AppCompatActivity {
                 }
                 String successMsg = "Post successful!";
                 Toast.makeText(PostActivity.this, successMsg, Toast.LENGTH_SHORT).show();
+
+                // Clear the edit text view description and image view.
                 etDescription.setText("");
+                ivImagePost.setImageResource(0);
             }
         });
 
